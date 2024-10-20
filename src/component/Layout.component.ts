c7bb06992e70622eb3925ab0ee88aefb36e456ff
AashishKumar Yadav <yadavaashish2709@gmail.com>
2024-10-20 12:13:33 +0530

    added achivements

--- src/component/Layout.component.ts
+++ src/component/Layout.component.ts
@@ -183,6 +183,24 @@ import { RouterLink, RouterOutlet } from '@angular/router';
                 >
               </a>
             </li>
+            <li>
+              <a
+                href="/achievements"
+                class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group"
+              >
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 -960 960 960"
+                  fill="currentColor"
+                >
+                  <path
+                    d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+                  />
+                </svg>
+                <span class="flex-1 ms-3 whitespace-nowrap">Achievements</span>
+              </a>
+            </li>
           </ul>
         </div>
       </aside>
