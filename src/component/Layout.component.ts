1bf04d28b8dc51ef5e9855f0f165b45a18a43847
amrohan <rohanxsalunkhe@gmail.com>
2024-10-20 13:25:08 +0530

    add notification

--- src/component/Layout.component.ts
+++ src/component/Layout.component.ts
@@ -43,73 +43,90 @@ import { RouterLink, RouterOutlet } from '@angular/router';
                 >
               </a>
             </div>
-            <div class="flex items-center">
-              <div class="flex items-center ms-3">
-                <div>
-                  <button
-                    type="button"
-                    class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
-                    aria-expanded="false"
-                    data-dropdown-toggle="dropdown-user"
-                  >
-                    <span class="sr-only">Open user menu</span>
-                    <img
-                      class="w-8 h-8 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                      alt="user photo"
-                    />
-                  </button>
-                </div>
-                <div
-                  class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow "
-                  id="dropdown-user"
-                >
-                  <div class="px-4 py-3" role="none">
-                    <p
-                      class="text-sm text-gray-900 dark:text-white"
-                      role="none"
+            <div class="flex justify-end items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-bell"
+              >
+                <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+                <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+              </svg>
+              <div class="flex items-center">
+                <div class="flex items-center ms-3">
+                  <div>
+                    <button
+                      type="button"
+                      class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
+                      aria-expanded="false"
+                      data-dropdown-toggle="dropdown-user"
                     >
-                      Neil Sims
-                    </p>
-                    <p
-                      class="text-sm font-medium text-gray-900 truncate"
-                      role="none"
-                    ></p>
+                      <span class="sr-only">Open user menu</span>
+                      <img
+                        class="w-8 h-8 rounded-full"
+                        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                        alt="user photo"
+                      />
+                    </button>
                   </div>
-                  <ul class="py-1" role="none">
-                    <li>
-                      <a
-                        routerLink="/home"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
-                        role="menuitem"
-                        >Home</a
+                  <div
+                    class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow "
+                    id="dropdown-user"
+                  >
+                    <div class="px-4 py-3" role="none">
+                      <p
+                        class="text-sm text-gray-900 dark:text-white"
+                        role="none"
                       >
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
-                        role="menuitem"
-                        >Settings</a
-                      >
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
-                        role="menuitem"
-                        >Earnings</a
-                      >
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
-                        role="menuitem"
-                        >Sign out</a
-                      >
-                    </li>
-                  </ul>
+                        Neil Sims
+                      </p>
+                      <p
+                        class="text-sm font-medium text-gray-900 truncate"
+                        role="none"
+                      ></p>
+                    </div>
+                    <ul class="py-1" role="none">
+                      <li>
+                        <a
+                          routerLink="/home"
+                          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                          role="menuitem"
+                          >Home</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                          role="menuitem"
+                          >Settings</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                          role="menuitem"
+                          >Earnings</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                          role="menuitem"
+                          >Sign out</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
